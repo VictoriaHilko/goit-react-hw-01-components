03010a02a0ab47a66c37f27b0869ff01164fb3fb
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-09-14 16:45:08 +0300

    add PropTypes

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -6,9 +6,21 @@ import { FriendListItem } from './FriendListItem';
 export const FriendList = ({ friends }) => {
     return (
         <ul className={css.friendsList}>
-            {friends.map(({ avatar, name, isOnline ,id }) => {
-                    return <FriendListItem avatar={avatar} name={name} isOnline={isOnline} key={id} />;
-                })}
+            {friends.map(({ avatar, name, isOnline, id }) => {
+                return <FriendListItem avatar={avatar} name={name} isOnline={isOnline} key={id} />;
+            })}
         </ul>
     );
+};
+
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(
+        PropTypes.shape({
+            avatar: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            isOnline: PropTypes.bool.isRequired,
+            id: PropTypes.number.isRequired,
+        }).isRequired
+    ).isRequired,
 };
