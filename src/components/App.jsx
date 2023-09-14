30c8a250f0b3b39c9cf9ec454a61731f0a4b0740
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-09-14 15:46:14 +0300

    task 2 added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-import { Profile } from "./Profile";
+import { Profile } from './Profile';
 import user from '../data/user.json';
+
+import { Statistics } from './Statistics';
+import statistics from '../data/statistics.json';
 
 
 export const App = () => {
   return (
     <>
-      <Profile 
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics
+        title="Upload stats"
+        stats={statistics}
       />
     </>
   );
---
+++ src/components/Statistics.jsx
@@ -0,0 +1,33 @@
+import css from './Statistics.module.css';
+import PropTypes from "prop-types";
+
+
+export const Statistics = ({ title, stats }) => {
+    return (
+        <section className={css.statistics}>
+            <h2 className={css.title}>{title}</h2>
+
+            <ul className={css.statsList}>
+                {stats.map(({ label, percentage, id }) => {
+                    return (
+                        <li className={css.statsItem} key={id}>
+                            <span className={css.statsName}>{label}</span>
+                            <span className={css.percentage}>{percentage}%</span>
+                        </li>
+                    );
+                })}
+            </ul>
+        </section>
+    );
+};
+
+Statistics.propTypes = {
+    title: PropTypes.string.isRequired,
+    stats: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired,
+        }).isRequired
+    ).isRequired
+};
