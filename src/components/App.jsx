3f95f6d58c02145e39bbfbd2c17095865d7a1e78
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-09-14 17:15:55 +0300

    add folders for modules

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import { Profile } from './Profile';
+import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
 
-import { Statistics } from './Statistics';
+import { Statistics } from './Statistics/Statistics';
 import statistics from '../data/statistics.json';
 
-import { FriendList } from './FriendList';
+import { FriendList } from './FriendsList/FriendList';
 import friends from '../data/friends.json';
 
-import { TransactionHistory } from './TransactionHistory';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import transactions from '../data/transactions.json';
 
 
